2e0f4857d001d2bab9b6c3bb2b1af8753a415ced
ouranoshong <ouranoshong@outlook.com>
2016-02-16 11:18:18 +0800

    Change webpack work with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,32 @@
 var gulp = require("gulp");
-var webpack = require("webpack-stream");
+var gutil = require("gulp-util");
+var webpack = require("webpack");
+var gulpPlugins = require("gulp-load-plugins")();
 var browserSync = require('browser-sync').create();
+var WebpackDevServer = require("webpack-dev-server");
 var webpackConfig = require("./webpack.config.js");
+var path = require('path');
+
+var buildPath = path.resolve(__dirname, './build');
+var sourcePath = path.resolve(__dirname, './src');
 
 gulp.task('serve', ["webpack:build-dev"], function() {
+
     browserSync.init({
-        server: "./dist"
+        server: buildPath,
+        open: false
     });
 
     gulp.watch(["src/**/*"], ["webpack:build-dev"]).on('change', browserSync.reload);
 });
 
-gulp.task("default", ['serve']);
+gulp.task("default", ["serve"]);
 
 gulp.task("build-dev", ["webpack:build-dev"], function() {
     gulp.watch(["src/**/*"], ["webpack:build-dev"]);
 });
 
+// Production build
 gulp.task("build", ["webpack:build"]);
 
 gulp.task("webpack:build", function(callback) {
@@ -30,24 +40,57 @@ gulp.task("webpack:build", function(callback) {
             }
         }),
         new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.UglifyJsPlugin()
+        new webpack.optimize.UglifyJsPlugin(),
     );
 
-    return gulp.src('src/main.js')
-        .pipe(webpack(myConfig))
-        .pipe(gulp.dest('dist/'));
+    // run webpack
+    webpack(myConfig, function(err, stats) {
+        if (err) throw new gutil.PluginError("webpack:build", err);
+        gutil.log("[webpack:build]", stats.toString({
+            colors: true
+        }));
+        callback();
+    });
 });
 
 gulp.task("webpack:build-dev", function(callback) {
-    // modify some webpack config options
     var myDevConfig = Object.create(webpackConfig);
     myDevConfig.devtool = "sourcemap";
     myDevConfig.debug = true;
 
     // create a single instance of the compiler to allow caching
     var devCompiler = webpack(myDevConfig);
+    // run webpack
+    devCompiler.run(function(err, stats) {
+        if (err) throw new gutil.PluginError("webpack:build-dev", err);
+        gutil.log("[webpack:build-dev]", stats.toString({
+            colors: true
+        }));
+        callback();
+    });
+});
 
-    return gulp.src('src/main.js')
-        .pipe(webpack(myDevConfig))
-        .pipe(gulp.dest('dist/'));
+gulp.task("webpack-dev-server", function(callback) {
+    // modify some webpack config options
+    var myConfig = Object.create(webpackConfig);
+    myConfig.devtool = "eval";
+    myConfig.debug = true;
+
+    // Start a webpack-dev-server
+    new WebpackDevServer(webpack(myConfig), {
+        publicPath: "/" + myConfig.output.publicPath,
+        stats: {
+            colors: true
+        }
+    }).listen(8080, "localhost", function(err) {
+        if (err) throw new gutil.PluginError("webpack-dev-server", err);
+        gutil.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
+    });
+});
+
+gulp.task('clean', function() {
+    return gulp.src("src/**/*", {
+            read: false
+        })
+        .pipe(gulpPlugins.destClean(buildPath));
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,29 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-var srcpath = path.resolve(__dirname,'./src');
+var srcpath = path.resolve(__dirname, './src');
 
 module.exports = {
-  entry: {
-    bundle: ['./src/main.js']
-  },
-  output: {
-    path: path.resolve(__dirname,'./build'),
-    filename: '[name].js'
-  },
-  module: {
-    loaders:[
-      { test: /\.js[x]?$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' },
-      { test: /\.css$/, loader: 'style-loader!css-loader?modules' }
+    entry: {
+        bundle: ['./src/main.js']
+    },
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js[x]?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader?presets[]=es2015&presets[]=react'
+        }, {
+            test: /\.css$/,
+            loader: 'style-loader!css-loader?modules'
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(srcpath, 'index.html')
+        })
     ]
-  }
 };
