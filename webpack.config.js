0ab55ef7b6c2bb7b76d923e4d0c0b2e2c932d4a1
hong <ouranoshong@outlook.com>
2016-02-20 15:05:16 +0800

    Add react hot loader

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,8 @@ module.exports = {
         loaders: [{
             test: /\.js[x]?$/,
             exclude: /node_modules/,
-            loader: 'babel-loader?presets[]=es2015&presets[]=react'
+            loader: 'react-hot-loader!babel-loader?presets[]=es2015&presets[]=react',
+	    include: scrpath
         }, {
             test: /\.css$/,
             loader: 'style-loader!css-loader?modules'
